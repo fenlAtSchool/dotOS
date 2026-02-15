fa38ecb611caca56de1a63832f1bb7256dca556b
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2026-02-14 17:51:58 -0800

    Refactor async module with updated Thread class

---
+++ src/modules/async.js
@@ -0,0 +1,47 @@
+/*
+Sample code:
+
+a = new Thread(function(){
+  api.log('Hi!')
+  yield* this.await(awaitUserInput())
+  api.log('Bye')
+})
+*/
+let m = {
+info: {
+  name: 'async',
+  version: '1.0.0',
+  source: 'github.com/tendergalaxy/dotOS/blob/main/src/modules/async.js'
+},
+callbacks: {
+
+onLoad(){
+  globalThis.Thread = new class{
+    constructor(func){
+      this.task = func
+      this.task()
+    }
+    tick(){
+      let value = this.task.next()
+      return TS.scheduleFirstUnused(() => (this.tick()))
+    }
+  }
+  globalThis.threadLibs = {
+    sleep(ms){
+      yield* threadLibs.sleep_internal(TS.tick + ms)
+    },
+    sleep_internal(del){
+      if(TS.tick < del){
+        yield
+      }
+    },
+    waitUntil(condition){
+      if(!condition){
+        yield
+      }
+    }
+  }
+}
+
+}
+}
