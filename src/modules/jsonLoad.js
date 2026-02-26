453c85c026a707f1531349847eb233cb8bca3204
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2026-02-25 20:41:06 -0800

    Create jsonLoad.js

---
+++ src/modules/jsonLoad.js
@@ -0,0 +1,16 @@
+{
+  info: {
+    name: 'jsonLoad',
+    type: 'os',
+    version: '1.0.0',
+    source: 'github.com/tendergalaxy/dotOS/blob/main/src/modules/jsonLoad.js',
+    requirements: []
+  },
+  callbacks: {
+    onLoad(){
+      globalThis.JSON.loadFile = function*(f){
+        return eval('let obj = ' + FS.async.getFile(f) + '; obj')
+      }
+    }
+  }
+}
