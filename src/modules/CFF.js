a3898d34a8e322e1fefcd866b3a21edfca5507c3
fenlAtSchool <alechan32@nuevaschool.org>
2026-03-02 12:36:09 -0800

    Add CFF module with info and onLoad callback

---
+++ src/modules/CFF.js
@@ -0,0 +1,17 @@
+{
+  info: {
+    name: 'CFF',
+    type: 'os',
+    version: '1.0.0',
+    source: 'github.com/fenlAtSchool/dotOS/blob/main/src/modules/CFF.js',
+    requirements: ['jsonLoad']
+  },
+  callbacks: {
+    onLoad(){
+      globalThis.CFF = class {
+        constructor(){}
+        setModule()
+      }
+    }
+  }
+}
