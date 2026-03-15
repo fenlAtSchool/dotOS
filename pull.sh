112403cec581137cc3e5538832c44a5a5c8e74cc
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2026-03-14 18:25:17 -0700

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 cd "$(dirname "$0")" || exit
 echo "Updating repo..."
-git pull origin main
+git pull origin main || git reset --hard origin/main
 echo "Building dotOS…"
 pip install -r requirements.txt
 python3 build.py
